1d8e62f5b9e255f135d91bad5cbe0fbccb706003
Madhukar yerraguntla <mycroft@Madhukars-MacBook-Air.local>
2024-12-25 11:49:37 +0530

    initial ideas for normalization

--- src/AST/Normalization.hs
+++ src/AST/Normalization.hs
@@ -1,1 +1,19 @@
 module AST.Normalization where
+
+import AST.NAST
+import AST.AST
+import Data.Foldable (foldlM, foldrM)
+import Control.Monad.State (StateT(runStateT),
+      MonadIO(liftIO),
+      MonadState(get,put),
+      MonadTrans(lift))
+import Control.Monad.Except (ExceptT(..), runExceptT, throwError, catchError)
+import Control.Monad (forM)
+
+data NormState = NormState {
+    freshvar :: Int,
+    id_list  :: [String],
+    stmts    :: [NStmt]
+                           } deriving (Eq, Show)
+
+type Normalize a = ExceptT String (StateT NormState IO) a
