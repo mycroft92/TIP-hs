0fb9d5117bc415bb8af45d7356b6455aed4927d9
Madhukar yerraguntla <mycroft@Madhukars-MacBook-Air.local>
2025-01-13 09:24:13 +0530

    wip unification

--- src/Solvers/Unification.hs
+++ src/Solvers/Unification.hs
@@ -1,6 +1,9 @@
 module Solvers.Unification where
 
 import AST.AST (Type (..), TypeVar)
+import qualified Data.Map as Map
+
+type Subst = Map.Map TypeVar Type
 
 vars' :: Type -> [TypeVar]
 vars' INT = []
@@ -8,3 +11,9 @@ vars' (Var i) = [i]
 vars' (Points t) = vars' t
 vars' (Arrow args ret) = concatMap vars' (ret : args)
 vars' (Mu t typ) = filter (/= t) (vars' typ)
+
+-- Need occurs check, apply substs to a term, union substs routines
+occursCheck :: TypeVar -> Type -> Bool
+occursCheck = undefined
+
+-- unify' ::
