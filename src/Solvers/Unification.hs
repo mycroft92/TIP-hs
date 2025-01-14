7c3a42a93f99acf409c1f1d13e2852384bb72092
Madhukar yerraguntla <mycroft@Madhukars-MacBook-Air.local>
2025-01-14 13:16:00 +0530

    unification wip

--- src/Solvers/Unification.hs
+++ src/Solvers/Unification.hs
@@ -31,6 +31,19 @@ occursCheck v (Arrow args) = checkargs
             args
 occursCheck v (Mu v' t) = (v /= v') && occursCheck v t
 
+-- implements substition v/x in t
+-- not checking capturing free variables
+subst :: Type -> TypeVar -> Type -> Type
+subst t@(Var x') x v
+    | x' == x = v
+    | otherwise = t
+subst INT _ _ = INT
+subst (Points t) x v = Points (subst t x v)
+subst (Arrow args) x v = Arrow (map (\t -> subst t x v) args)
+subst (Mu x' t) x v
+    | x == x' = Mu x' t
+    | otherwise = Mu x' (subst t x v)
+
 -- findSubst :: TypeVar -> Subst -> Type
 -- findSubst v ss =
 --     case Map.lookup v ss of
