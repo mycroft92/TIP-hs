a7aca4c7e6c5fd699a82dc0ddae219e54d70da6b
Madhukar yerraguntla <mycroft@Madhukars-MacBook-Air.local>
2025-01-16 23:45:09 +0530

    moving slowly

--- src/Solvers/Unification.hs
+++ src/Solvers/Unification.hs
@@ -44,27 +44,4 @@ subst (Mu x' t) x v
     | x == x' = Mu x' t
     | otherwise = Mu x' (subst t x v)
 
--- findSubst :: TypeVar -> Subst -> Type
--- findSubst v ss =
---     case Map.lookup v ss of
---         Nothing -> Var v
---         Just (Var v') -> findSubst v' ss
---         Just t -> t
---
--- -- flying blind here, I don't know the correct approach
--- applySubst :: Subst -> Type -> Type
--- applySubst ss (Var n) = findSubst n ss
--- applySubst _ INT = INT
--- applySubst ss (Points t) = Points (applySubst ss t)
--- applySubst ss (Arrow args) = Arrow (map (applySubst ss) args)
--- applySubst ss (Mu v' t) = Mu v' (applySubst ss' t)
---   where
---     ss' = Map.delete v' ss
-
--- unify' :: Type -> Type -> Subst -> Either (String, String) Subst
--- unify' t1@(Var v1) t2@(Var v2) ss =
---     let v1r = findSubst v1 ss in
---     let v2r = findSubst v2 ss in
---     case v2r of
---         Var _ -> Right v1r
---         _ ->  Right v2r
+-- close ::
