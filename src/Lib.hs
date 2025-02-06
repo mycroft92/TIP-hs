c1e7dbe00ff4309ae2ea5b02dbe8aa2cf7ee8d5a
Madhukar yerraguntla <mycroft@Madhukars-MacBook-Air.local>
2025-02-06 22:49:06 +0530

    typechecking in progress, currently buggy

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,7 @@ module Lib where
 import AST.AST
 import AST.NAST
 import AST.Normalization (normalizeFunction)
+import Analysis.TypeChecker (TypeState (..), runTypeChecker)
 import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Parser.Lexer as L
 import Parser.Parser (parse)
@@ -16,6 +17,11 @@ runFile s = do
         Left err -> putStr (show err) >> return 1
         Right exp -> do
             -- print exp
+            putStrLn "Running Type checker on parse output"
+            outty <- runTypeChecker exp
+            case outty of
+                Left err -> putStrLn err
+                Right ts -> print $ show (globalenv ts)
             print "Parse finished, normalizing:"
             funcs <- mapM normalizeFunction exp
             print funcs
